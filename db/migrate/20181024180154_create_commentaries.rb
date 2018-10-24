9e99415580d60135a695fd0ffe13d6dbf055f7b7
vrattanasamay <victor.rattanasamay@gmail.com>
2018-10-24 20:29:24 +0200

    pinterest

---
+++ db/migrate/20181024180154_create_commentaries.rb
@@ -0,0 +1,8 @@
+class CreateCommentaries < ActiveRecord::Migration[5.2]
+  def change
+    create_table :commentaries do |t|
+      t.belongs_to :user, index: true
+      t.timestamps
+    end
+  end
+end
